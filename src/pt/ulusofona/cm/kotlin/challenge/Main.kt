b677e14faedb89bebb7d1b21d61758244700da96
NettoBaptistella-21907847 <netto.e.baptistella@gmail.com>
2021-02-22 11:59:59 +0000

    Edit

--- src/pt/ulusofona/cm/kotlin/challenge/Main.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/Main.kt
@@ -1,7 +1,18 @@
 package pt.ulosofona.cm.kotlin.challenge
 
+import pt.ulosofona.cm.kotlin.challenge.models.Bicicleta
+import pt.ulosofona.cm.kotlin.challenge.models.Pessoa
+import pt.ulosofona.cm.kotlin.challenge.models.Veiculo
+import java.util.*
+
+
 fun main() {
 
+    val pessoa = Pessoa("Netto", Date())
+    /*val bike: Veiculo = Bicicleta("01")
+    bike.moverPara(10, 10)
+    bike.moverPara(5,5)*/
+    println(pessoa)
 
 
 }
--- src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/interfaces/Ligavel.kt
@@ -1,6 +1,5 @@
 package pt.ulosofona.cm.kotlin.challenge.interfaces
 
-import pt.ulosofona.cm.kotlin.challenge.models.Carro
 import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoDesligadoException
 import pt.ulusofona.cm.kotlin.challenge.exceptions.VeiculoLigadoException
 import kotlin.jvm.Throws
